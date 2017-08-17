4255554e0b8cdeb36984db8e78c6193f4c3783c5
Alex Stolzoff <alexstolznut@hotmail.com>
2017-08-17 07:28:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,44 +1,44 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray(array[], el){
-    const array = array[];
-    const newArray = [el, ...array];
-    return newArray;
-}
-
-function destructivelyAddElementToBeginningOfArray(array[], el){
-  array[].unshift(el);
-  return array[];
-}
-
-function addElementToEndOfArray(array[], el){
-  const array = array[];
-    const newArray = [...array, el];
-    return newArray;
-}
-
-function destructivelyAddElementToEndOfArray(array, el){
-  array.push(el);
-  return array[];
-}
-
-function accessElementInArray(array[], index){
-    return array[index];
-}
-
-function removeElementFromBeginningOfArray(array){
-  var arraySliced = array.slice(1);
-  return arraySliced;
-}
-
-function destructivelyRemoveElementFromEndOfArray(array;){
-    array.pop();
-    return array;
-
-}
-
-function removeElementFromEndOfArray(array){
-  array.slice(0, array.length-1);
-  return array;
-}
-destructivelyAddElementToEndOfArray(chocolateBars, "reeses");
+// function addElementToBeginningOfArray(array[], el){
+//     const array = array[];
+//     const newArray = [el, ...array];
+//     return newArray;
+// }
+// 
+// function destructivelyAddElementToBeginningOfArray(array[], el){
+//   array[].unshift(el);
+//   return array[];
+// }
+// 
+// function addElementToEndOfArray(array[], el){
+//   const array = array[];
+//     const newArray = [...array, el];
+//     return newArray;
+// }
+// 
+// function destructivelyAddElementToEndOfArray(array, el){
+//   array.push(el);
+//   return array[];
+// }
+// 
+// function accessElementInArray(array[], index){
+//     return array[index];
+// }
+// 
+// function removeElementFromBeginningOfArray(array){
+//   var arraySliced = array.slice(1);
+//   return arraySliced;
+// }
+// 
+// function destructivelyRemoveElementFromEndOfArray(array;){
+//     array.pop();
+//     return array;
+// 
+// }
+// 
+// function removeElementFromEndOfArray(array){
+//   array.slice(0, array.length-1);
+//   return array;
+// }
+// destructivelyAddElementToEndOfArray(chocolateBars, "reeses");
