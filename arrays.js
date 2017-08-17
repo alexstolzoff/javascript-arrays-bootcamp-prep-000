24c39e8c6cc208fdb97141a61eed4ac009fd607a
Alex Stolzoff <alexstolznut@hotmail.com>
2017-08-17 06:45:43 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,34 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array[], el){
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(array[], el){
+  array[].unshift(el);
+}
+
+function addElementToEndOfArray(array[], el){
+  
+}
+
+function destructivelyAddElementToEndOfArray(array, el){
+  array.push(el);
+  return array[];
+}
+
+function accessElementInArray(array[], index){
+  
+}
+
+function removeElementFromBeginningOfArray(array){
+  var arraySliced = array.slice(1);
+  return arraySliced;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array;){
+    array.pop();
+    return array;
+  
+}
+destructivelyAddElementToEndOfArray(chocolateBars, "reeses");
